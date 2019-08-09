7ed2b03e135062e49ba10dfb97a58fce5d2adfc5
raladev <aladev.r@gmail.com>
2019-08-09 23:33:32 +0300

    update sync example

--- sync_ws.py
+++ sync_ws.py
@@ -19,6 +19,7 @@ class SyncWsConnection:
         while True:
             try:
                 self.ws = await websockets.connect(self._url)
+                print('connection established')
                 #some init messages
                 return True
             except TimeoutError as e:
@@ -34,7 +35,6 @@ class SyncWsConnection:
     async def async_send(self, msg='hell-send'):
         try:
             await self.ws.send(msg)
-            await asyncio.sleep(2)
             return True
         except websockets.ConnectionClosedError as e:
             print('ConnectionClosedError Exception in send: {}'.format(e))
@@ -43,11 +43,50 @@ class SyncWsConnection:
             print('Unexpected Exception in send: {}'.format(e))
             await self.async_connect()
 
+    def read(self):
+        return self._loop.run_until_complete(self.async_read())
+
+    async def async_read(self):
+        try:
+            return await self.ws.recv()
+        except websockets.ConnectionClosedError as e:
+            print('ConnectionClosedError Exception in send: {}'.format(e))
+            await self.async_connect()
+        except Exception as e:
+            print('Unexpected Exception in send: {}'.format(e))
+            await self.async_connect()
+
+    def send_and_get_answer(self, msg='do something'):
+        return self._loop.run_until_complete(self.async_send_and_get_answer(msg))
+
+    async def async_send_and_get_answer(self, msg='do something'):
+        await self.async_send(msg)
+        answer = None
+        #read until we get correct message from the queue
+        while answer != msg:
+            answer = await self.async_read()
+        return answer
+
     def ping(self):
         return self._loop.run_until_complete(self._ping())
 
     async def _ping(self):
         pong_waiter = await self.ws.ping()
+        answer = await pong_waiter
         print(await pong_waiter)
-        return True
+        return answer
+
+if __name__ == "__main__":
+        ws = SyncWsConnection()
+        ws.connect()
+        print(ws.send_and_get_answer('gogen solntsev'))
+        if ws.send('message without answer'):
+            print('msg without answer sended')
+        if ws.send('second message without answer'):
+            print('second msg without answer sended')
+        # read first msg from queue
+        print(ws.read() + ' readed')
+        # send msg and read from queue until get correct answer (second msg without answer will be read, skipped and lost)
+        print(ws.send_and_get_answer('b  bs'))
+
 
